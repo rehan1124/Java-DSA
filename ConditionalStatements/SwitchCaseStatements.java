7fca34cfcc8d4c4905941b27d38b939906623bd5
rehan1124 <rehan.11.24@gmail.com>
2024-05-09 17:11:48 +0530

    Switch case statements

--- ConditionalStatements/SwitchCaseStatements.java
+++ ConditionalStatements/SwitchCaseStatements.java
@@ -1,7 +1,24 @@
 package ConditionalStatements;
 
+import java.util.Scanner;
+
 public class SwitchCaseStatements {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number from 1 to 3:");
+        int a = sc.nextInt();
+        switch (a) {
+            case 1:
+                System.out.println("Hello");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Bonjour");
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
     }
 }
