9c22619273e755ec000658b21ce9bf1243388992
rehan1124 <rehan.11.24@gmail.com>
2024-05-25 15:25:44 +0530

    For loop

---
+++ ConditionalStatements/ForLoop.java
@@ -0,0 +1,9 @@
+package ConditionalStatements;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.printf("Hello world %s time(s) using for-loop%n", i);
+        }
+    }
+}
